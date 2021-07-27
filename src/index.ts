9a857e1b4f5103a8ab071d681bb06a8b74b50f0b
bakjonghun <bakjonghun@hikick.kr>
2021-07-28 04:22:17 +0900

    change: single thread to multi thread

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,6 @@ export const handler: Handler = async (event, context) => {
     MongoDB.init(),
   ]);
 
-  const duplicateCode: string[] = [];
   logger.info(
     `[킥보드] 파이어베이스 킥보드 갯수는 총 ${kickboardDocs.size}개 입니다.`
   );
@@ -34,76 +33,77 @@ export const handler: Handler = async (event, context) => {
     `[킥보드] 몽고디비 킥보드 갯수는 총 ${kickboards.length}개 입니다.`
   );
 
-  for (const kickboardDoc of kickboardDocs.docs) {
-    const {
-      id: kickboardId,
-      code: kickboardCode,
-      can_ride: canRide,
-      deploy,
-    } = kickboardDoc.data();
-    const displayName = `${kickboardCode}(${kickboardId})`;
-    if (kickboardCode.length !== 6) {
-      logger.warn(`[킥보드] ${displayName} 올바르지 않은 킥보드 코드입니다.`);
-      continue;
-    }
+  const duplicateCode: string[] = [];
+  const functions = kickboardDocs.docs.map((kickboardDoc) => {
+    return async () => {
+      const {
+        id: kickboardId,
+        code: kickboardCode,
+        can_ride: canRide,
+        deploy,
+      } = kickboardDoc.data();
+      const displayName = `${kickboardCode}(${kickboardId})`;
+      if (kickboardCode.length !== 6) {
+        logger.warn(`[킥보드] ${displayName} 올바르지 않은 킥보드 코드입니다.`);
+        return;
+      }
 
-    if (duplicateCode.includes(kickboardCode)) {
-      logger.warn(`[킥보드] ${displayName} 이미 처리된 킥보드 코드입니다.`);
-      continue;
-    }
+      if (duplicateCode.includes(kickboardCode)) {
+        logger.warn(`[킥보드] ${displayName} 이미 처리된 킥보드 코드입니다.`);
+        return;
+      }
 
-    duplicateCode.push(kickboardCode);
-    const kickboard = await kickboards.find(
-      (e) => e.kickboardCode === kickboardCode
-    );
-
-    let mode = KickboardMode.READY;
-    if (!canRide) mode = KickboardMode.INUSE;
-    if (!deploy) mode = KickboardMode.COLLECTED;
-    if (kickboard) {
-      const bypassMode = [
-        KickboardMode.UNREGISTERED,
-        KickboardMode.BROKEN,
-        KickboardMode.DISABLED,
-      ];
-
-      if (kickboard.mode === mode || bypassMode.includes(kickboard.mode))
-        continue;
-
-      const changed =
-        KickboardMode[kickboard.mode] + ' -> ' + KickboardMode[mode];
-      logger.info(
-        `[킥보드] ${displayName} 이미 존재하여 상태만 변경하였습니다. (${changed})`
+      duplicateCode.push(kickboardCode);
+      const kickboard = await kickboards.find(
+        (e) => e.kickboardCode === kickboardCode
       );
 
+      let mode = KickboardMode.READY;
+      if (!canRide) mode = KickboardMode.INUSE;
+      if (!deploy) mode = KickboardMode.COLLECTED;
+      if (kickboard) {
+        const bypassMode = [
+          KickboardMode.UNREGISTERED,
+          KickboardMode.BROKEN,
+          KickboardMode.DISABLED,
+        ];
+
+        if (kickboard.mode === mode || bypassMode.includes(kickboard.mode))
+          return;
+
+        const changed =
+          KickboardMode[kickboard.mode] + ' -> ' + KickboardMode[mode];
+        logger.info(
+          `[킥보드] ${displayName} 이미 존재하여 상태만 변경하였습니다. (${changed})`
+        );
+
+        kickboard.mode = mode;
+        await kickboard.save();
+        return;
+      }
+
+      await KickboardModel.create({
+        kickboardId,
+        kickboardCode,
+        franchiseId,
+        regionId,
+        mode,
+        lost: null,
+        helmetId: null,
+        maxSpeed: null,
+        collect: null,
+        disconnectedAt: null,
+      });
+
+      logger.info(`[킥보드] ${displayName} 킥보드를 생성하였습니다.`);
       await Webhook.send(
-        `[파이어베이스 싱크] ${displayName} 이미 존재하여 상태만 변경하였습니다. (${changed})`
+        `[파이어베이스 싱크] ${displayName} 킥보드를 생성하였습니다.`
       );
+    };
+  });
 
-      kickboard.mode = mode;
-      await kickboard.save();
-      continue;
-    }
-
-    await KickboardModel.create({
-      kickboardId,
-      kickboardCode,
-      franchiseId,
-      regionId,
-      mode,
-      lost: null,
-      helmetId: null,
-      maxSpeed: null,
-      collect: null,
-      disconnectedAt: null,
-    });
-
-    logger.info(`[킥보드] ${displayName} 킥보드를 생성하였습니다.`);
-    await Webhook.send(
-      `[파이어베이스 싱크] ${displayName} 킥보드를 생성하였습니다.`
-    );
-  }
-
+  while (functions.length)
+    await Promise.all(functions.splice(0, 50).map((f) => f()));
   const processTime = `${(Date.now() - startTime).toLocaleString()}ms`;
   logger.info(`[시스템] 시스템 처리가 완료되었습니다. ${processTime}`);
 };
